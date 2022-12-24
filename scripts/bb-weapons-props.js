c908b290ee1d699fb423e25aa34949d57a28ac13
Don Macaron <donmacaron95@gmail.com>
2022-12-23 20:14:41 -0400

    bjodgndg

--- scripts/bb-weapons-props.js
+++ scripts/bb-weapons-props.js
@@ -11,12 +11,4 @@ Hooks.on("init", async function () {
     lngreach: "Long Reach",
     unwieldy: "Unwieldy"
   };
-  CONFIG.DND5E.weaponIds = {
-  ...CONFIG.DND5E.weaponIds,
-  // Добавь ID предметов, являющихся базовым вариантом оружия
-    trickweap: "eBxvVLR7jo35G75f",
-    trickfirearm: "AgfoTQCMQ5uJH7NU",
-    firearm: "vJ9KzYD03yIAfhLl",
-    choir: "t1G8fkvutUGvqAbT"
-  };
 });
